6dd0947d2394edcffb507eada21c58ac6749f293
macuser <macuser@macusers-MacBook-Pro.local>
2020-11-29 17:49:49 +0200

    parse_cookie

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
-def parse(query: str) -> dict:
-    if query == '':
+def parse_cookie(query: str) -> dict:
+    if query == '': return {}
+    if query == ';': return {}
+    if query == '=': return {}
+    a = str(query).split(';')
+    if len(a) == 1:
         return {}
-        braek
-    if query.find('?') == -1:
-        return {}
-    b = str(query).split('?', 1)[1].split('&')
-    if b[-1] == '':
-        del b[-1]
+    if a[-1] == '':
+        del a[-1]
     t = 0
-    for i in range(len(b)):
-        if b[t] == '':
-           del b[t] 
-           t-= 1
-        t+= 1
-    d = dict(s.split('=', 1) for s in b)
+    for i in range(len(a)):
+        if a[t] == '':
+            del a[t]
+            t -= 1
+        t += 1
+    d = dict(s.split('=', 1) for s in a)
     return d
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
 # 1
-    assert parse('https://www.google.com/search?rlz=1C5CHFA_enUA894UA894&sxsrf=ALeKk02R6JovRZvSRzOapN7SNIGCCI3LMA%3A1606652092110&ei=vJDDX-GEBsWWjgb-267YBg&q=%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B0+http+&oq=%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B0+http+&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyBggAEBYQHjIGCAAQFhAeMgYIABAWEB5QyF5Y33Rgz3loAHAAeAGAAYEEiAHyLpIBBzMtNC44LjKYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab&ved=0ahUKEwjh0ui53aftAhVFi8MKHf6tC2sQ4dUDCA0&uact=5') == {'rlz': '1C5CHFA_enUA894UA894', 'sxsrf': 'ALeKk02R6JovRZvSRzOapN7SNIGCCI3LMA%3A1606652092110', 'ei': 'vJDDX-GEBsWWjgb-267YBg', 'q': '%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B0+http+', 'oq': '%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B0+http+', 'gs_lcp': 'CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyBggAEBYQHjIGCAAQFhAeMgYIABAWEB5QyF5Y33Rgz3loAHAAeAGAAYEEiAHyLpIBBzMtNC44LjKYAQCgAQGqAQdnd3Mtd2l6wAEB', 'sclient': 'psy-ab', 'ved': '0ahUKEwjh0ui53aftAhVFi8MKHf6tC2sQ4dUDCA0', 'uact': '5'}
+    assert parse_cookie('name=Dima;;age=28') == {'name': 'Dima', 'age': '28'}
 # 2
-    assert parse('http://shpargalkablog.ru') == {}
+    assert parse_cookie(';') == {}
 # 3
-    assert parse('https://www.google.com/search?q=%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D1%87%D0%B8%D0%BA+%D0%B0%D0%BD%D0%B3%D0%BB%D0%BE+-%D1%80%D1%83%D1%81%D1%81&rlz=1C5CHFA_enUA894UA894&oq=&aqs=chrome.0.35i39i362l8...8.232712111j0j15&sourceid=chrome&ie=UTF-8') == {'q': '%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D1%87%D0%B8%D0%BA+%D0%B0%D0%BD%D0%B3%D0%BB%D0%BE+-%D1%80%D1%83%D1%81%D1%81', 'rlz': '1C5CHFA_enUA894UA894', 'oq': '', 'aqs': 'chrome.0.35i39i362l8...8.232712111j0j15', 'sourceid': 'chrome', 'ie': 'UTF-8'}
+    assert parse_cookie('=') == {}
 # 4
-    assert parse('https://www.google.com/search?name=roma&&age=13') == {'name': 'roma', 'age': '13'}
+    assert parse_cookie('n=r;r=t=y;i=o') == {'n': 'r', 'r': 't=y', 'i': 'o'}
 # 5
-    assert parse('https://www.google.com/search?n==j&h=q') == {'n': '=j', 'h': 'q'}
+    assert parse_cookie(';;') == {}
 # 6
-    assert parse('jwidjd.com') == {}
+    assert parse_cookie('a') == {}
 # 7
-    assert parse('https://www.google.com/search??n=j&h=q') == {'?n': 'j', 'h': 'q'}
+    assert parse_cookie('*') == {}
 # 8
-    assert parse('https://www.google.com/search?n=j&h?=q') == {'n': 'j', 'h?': 'q'}
+    assert parse_cookie('a=a;') == {'a': 'a'}
 # 9
-    assert parse('') == {}
+    assert parse_cookie('m=n;;;p=r;p=e') == {'m': 'n', 'p': 'r', 'p': 'e'}
 # 10
-    assert parse('?') == {}
+    assert parse_cookie(' ') == {}
 
